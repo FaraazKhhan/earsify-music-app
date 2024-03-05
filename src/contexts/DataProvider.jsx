1c7afa27c8e525232ba3c703ab7a3bc4470747af
Faraaz Khan <codefaraaz@gmail.com>
2024-03-05 15:15:18 +0530

    aaded(core): config.json support

--- src/contexts/DataProvider.jsx
+++ src/contexts/DataProvider.jsx
@@ -4,19 +4,33 @@ export const DataContext = createContext();
 
 export const DataProvider = (props) => {
   const [data, setData] = useState(null);
-  const [query, setQuery] = useState("english");
+  const [query, setQuery] = useState(null);
 
-  const fetchData = async () => {
+  const getConfig = async () => {
+    const configFilePath = "/configs/config.json";
+    const response = await fetch(configFilePath);
+    return await response.json();
+  }
+
+  const fetchData = async (apiOrigin) => {
     const encodedQuery = encodeURI(query);
+    apiOrigin = apiOrigin.endsWith("/") ? apiOrigin : `${apiOrigin}/`
     const response = await fetch(
-      `https://saavn.me/search/songs?query=${encodedQuery}`
+      `${apiOrigin}search/songs?query=${encodedQuery}`
     );
     const data = await response.json();
     setData(data);
   };
 
   useEffect(() => {
-    fetchData();
+    window['earsifyCore']['config'] = await getConfig();
+    const { musicApiConfig: { initialQuery = '' } } = window['earsifyCore']['config'];
+    setQuery(initialQuery);
+  }, []);
+
+  useEffect(() => {
+    const { musicApiConfig: { origin = '' } } = window['earsifyCore']['config'];
+    fetchData(origin);
   }, [query]);
 
   return (
